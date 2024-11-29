2c277c09a3151cb10d29c91b31202515608cbb8d
YASH <supermenant@gmail.com>
2024-11-29 16:47:00 +0530

    PROGRAMS OF PYTHON
    Mypracticsed program

---
+++ PROGRAM1.py
@@ -0,0 +1,12 @@
+Python 3.13.0rc2 (tags/v3.13.0rc2:ec61006, Sep  6 2024, 22:13:49) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name="yash"
+>>> age=23
+>>> weight=89.6
+>>> print(type(name))
+<class 'str'>
+>>> 
+= RESTART: C:/Users/lenovo/AppData/Local/Programs/Python/Python313/PROGRAM1.py
+<class 'str'>
+<class 'int'>
+<class 'float'>
---
+++ programtask.py
@@ -0,0 +1,11 @@
+#my first program practiced
+#q1
+a=int(input("enter the name"))
+b=int(input("enter the name"))
+sum=a+b
+print(sum)
+#q2
+side=int(input("enter the side"))
+square=side*side
+print(square)
+      
